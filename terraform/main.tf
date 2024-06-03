7427157ab778a0ea40b79fd85e9254b7660a62cd
Alexandre Du Lorier <alexandre.dulorier2@sas.se>
2024-06-04 01:25:31 +0200

    fix casing for images

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,8 +16,27 @@ resource "azurerm_service_plan" "example" {
 }
 
 
-resource "azurerm_linux_web_app" "example" {
-  name                = "vp-example"
+resource "azurerm_linux_web_app" "vacation-planner-docs" {
+  name                = "vacation-planner-docs"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  service_plan_id     = azurerm_service_plan.example.id
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+  site_config {
+    always_on = false
+    application_stack {
+      docker_image_name   = "vacation-planner-docs:main"
+      docker_registry_url = "https://ghcr.io/temperantia"
+    }
+  }
+}
+
+resource "azurerm_linux_web_app" "vacation-planner-web" {
+  name                = "vacation-planner-web"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   service_plan_id     = azurerm_service_plan.example.id
